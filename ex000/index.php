8e842500c7366e86e1e87547266c6be7b3a8db25
Gabriel Batista <ryubiel@gmail.com>
2025-09-12 22:29:42 -0300

    ex 0 até o 4

---
+++ ex000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá, Mundo!</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Olá, Mundo! \u{1F30E}";
+        ?>
+    </h1>
+    <p>Meu primeiro código PHP</p>
+</body>
+</html>
---
+++ ex003/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+        // 0x = hexadecimal, 0b = binario, 0 = octal
+        // $num = 010;
+        // echo "O valor da variável é $num";
+
+        // $v = "1.5";
+        // var_dump($v);
+
+        $num = 3e2; // 3 x 10^2
+        echo "O valor da variável é $num";
+    ?>
+</body>
+</html>
