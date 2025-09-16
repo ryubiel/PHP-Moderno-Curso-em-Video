861b3e777168226841ac79f57153da5a105b8b0f
Gabriel Batista <ryubiel@gmail.com>
2025-09-16 13:49:40 -0300

    desafio 010

--- desafios/d010/index.php
+++ desafios/d010/index.php
@@ -9,37 +9,27 @@
 <body>
     <<?php 
         // Capturando os dados do Formulário Retroalimentado
-        $v1 = $_GET['v1'] ?? '';
-        $p1 = $_GET['p1'] ?? '';
-        $v2 = $_GET['v2'] ?? '';
-        $p2 = $_GET['p2'] ?? '';
- 
+        $aatual = date('Y');
+        $nasc = $_GET['nasc'] ?? 0;
+        $ano = $_GET['ano'] ?? $aatual;
+        
     ?>
     <main>
-        <h1>Médias aritméticas</h1>
+        <h1>Calculando a sua idade</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-            <label for="v1">1° Valor</label>
-            <input type="number" name="v1" id="v1" min="0" value="<?=$v1?>" required>
-            <label for="p1">1° Peso</label>
-            <input type="number" name="p1" id="p1" min="0" value="<?=$p1?>" required>
-            <label for="v2">2° Valor</label>
-            <input type="number" name="v2" id="v2" min="0" value="<?=$v2?>" required>
-            <label for="p2">2° Peso</label>
-            <input type="number" name="p2" id="p2" min="0" value="<?=$p2?>" required>
-            <input type="submit" value="Calcular Raízes">
+            <label for="nasc">Em que ano você nasceu?</label>
+            <input type="number" name="nasc" id="nasc" max="<?=$aatual?>" value="<?=$nasc?>" required>
+            <label for="ano">Quer saber sua idade em que ano? (atualmente estamos em <?=$aatual?>)</label>
+            <input type="number" name="ano" id="ano" min="0" value="<?=$ano?>" required>
+            <input type="submit" value="Qual será minha idade?">
         </form>
 
         <section>   
-            <h2>Cálculo das Médias</h2>
+            <h2>Resultado</h2>
             <?php 
-                $media = ($v1 + $v2) / 2;
-                $ponderada = ($v1 * $p1 + $v2 * $p2) / ($p1 + $p2);
+                $idade = $ano - $nasc;
 
-                echo "<p>Analisando os valores $v1 e $v2:</p>";
-                echo "<ul>
-                        <li>A <strong> Média Aritmética Simples</strong> entre os valores é igual a " . number_format($media, 2,",",".") . "</li>
-                        <li>A <strong> Média Aritmética Ponderada</strong> com pesos $p1 e $p2 é igual a " . number_format($ponderada, 2, ",", ".") . "</li>
-                    </ul>";
+                echo "<p>Quem nasceu em $nasc vai ter $idade anos em $ano!</p>";
             ?>
         </section>
     </main>
